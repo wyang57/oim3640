e163985d876e95235110fd7e2412a41a55fa4e65
bluemanta7 <bluemantarayberry@gmail.com>
2026-04-29 18:25:44 -0400

    Complete MP5 with working ONET API connection
    Integrating ONET API key

--- projects/mp5-job-quiz-2/client.py
+++ projects/mp5-job-quiz-2/client.py
@@ -1,13 +1,13 @@
 import requests
 
-API_KEY = "PENDING_APPROVAL"
+API_KEY = "acGmN-xDyg1-SYaW5-lyV7T"
 
 def fetch_careers(keyword):
     url = f"https://services.onetcenter.org/ws/online/occupations?keyword={keyword}&start=0&end=5"
     response = requests.get(url, auth=(API_KEY, ""))
 
     if response.status_code != 200:
-        return ["There was a problem with the request. The API key is still pending approval."]
+        return ["There was a problem with the request."]
 
     data = response.json()
     results = []
